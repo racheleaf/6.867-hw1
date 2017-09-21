658ee39835dd4fba6fe12b9450029290db000982
Alok Puranik <alokpuranik1@gmail.com>
2017-09-20 23:51:06 -0400

    Add gradient norm plotting

--- p1_gradient_descent.py
+++ p1_gradient_descent.py
@@ -1,10 +1,14 @@
 import numpy as np
 import math
 import random
+import matplotlib
+matplotlib.use('Gtk3Cairo')
+import matplotlib.pyplot as plt
+
 import loadParametersP1 as loadParams
 import loadFittingDataP1 as loadData
 
-def batch_descent(initial_guess, step_size, threshold, gradient):
+def batch_descent(initial_guess, step_size, threshold, gradient, track_history=False):
     '''
     initial_guess = vector
     step_size = number, the size of each step
@@ -12,23 +16,30 @@ def batch_descent(initial_guess, step_size, threshold, gradient):
     gradient = function computing gradient
     '''
     cur_var = initial_guess # list of iteratively computed coordinates
+    if track_history:
+        hist = []
     
     while True:
         gradient_at_cur_var = gradient(cur_var)
         # stops when the norm of the gradient falls below threshold
-        if np.linalg.norm(gradient_at_cur_var) < threshold:
+        gradnorm = np.linalg.norm(gradient_at_cur_var)
+        if track_history:
+            hist.append(gradnorm)
+        if gradnorm < threshold:
             break
         cur_var -= step_size * gradient_at_cur_var
 
-    return cur_var
+    return (cur_var, np.array(hist)) if track_history else cur_var
 
-def stochastic_descent(initial_guess, gradient, dataX, datay, lr=0.01, iters=10000):
+def stochastic_descent(initial_guess, gradient, dataX, datay, lr=0.01, iters=10000, track_history=False):
     '''
     initial_guess is a vector
     gradient is a function computing gradient at single data point
     returns stochastic gradient descent minimum
     '''
     cur_var = initial_guess
+    if track_history:
+        hist = np.zeros((iters,))
     
     for i in range(iters):
         total_data = len(datay)
@@ -37,13 +48,14 @@ def stochastic_descent(initial_guess, gradient, dataX, datay, lr=0.01, iters=100
         yi = datay[data_index]
         gradient_at_cur_var = gradient(xi, yi, cur_var)
         # stops when the norm of the gradient falls below threshold
+        hist[i] = np.linalg.norm(gradient_at_cur_var)
         # print(np.linalg.norm(gradient_at_cur_var))
         cur_var -= lr / math.sqrt(i+1) * gradient_at_cur_var
         if not i % 100:
             print("DEBUG: cur_var =", cur_var)
             print("DEBUG: gradient =", gradient_at_cur_var)
         
-    return cur_var
+    return cur_var, hist if track_history else cur_var
 
 def numerical_gradient(f, vector, h=1e-4):
     '''
@@ -117,7 +129,13 @@ def run_things():
     step_size = 0.01
     threshold = 0.25
 
-    print("BATCH DESCENT MIN:", batch_descent(quadBowlStart, step_size, threshold, lambda v: gradient_quad_bowl(quadBowlA, quadBowlb, v)))
+    quadBowlOpt, quadBowlHist = batch_descent(quadBowlStart, step_size, threshold, lambda v: gradient_quad_bowl(quadBowlA, quadBowlb, v), track_history=True)
+    plt.plot(quadBowlHist)
+    plt.title("Gradient of Quadratic")
+    plt.xlabel("Iteration")
+    plt.ylabel("Norm of Gradient")
+    plt.show()
+    print("BATCH DESCENT MIN:", quadBowlOpt)
     print("ACTUAL MIN:", np.linalg.inv(quadBowlA).dot(quadBowlb))
 
     print('ESTIMATED GRADIENT: {}'.format(numerical_gradient(lambda x: (0.5 * x.T.dot(quadBowlA.dot(x)) - quadBowlb.dot(x)), quadBowlStart)))
@@ -126,8 +144,14 @@ def run_things():
     # least square fitting problem
     X, y = loadData.getData()
     thetaGuess = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], dtype = "float64")
-    print("GRADIENT DESCENT MIN:", batch_descent(thetaGuess, step_size / 100, threshold, lambda v: gradient_lse(X, y, v)))
-    print("SGD MIN:", stochastic_descent(thetaGuess, gradient_lse_data_point, X, y, lr=step_size / 100000, iters=10000))
+    batch_min, hist = batch_descent(thetaGuess, step_size / 100, threshold, lambda v: gradient_lse(X, y, v), track_history=True)
+    print("GRADIENT DESCENT MIN:", batch_min)
+    plt.plot(hist)
+    plt.show()
+    sgd_opt, sgd_hist = stochastic_descent(thetaGuess, gradient_lse_data_point, X, y, lr=step_size / 100000, iters=10000, track_history=False)
+    print("SGD MIN:", sgd_opt)
+    # plt.plot(sgd_hist)
+    # plt.show()
 
 if __name__ == '__main__':
     run_things()
